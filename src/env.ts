e3e9da4844dfe6e6344ca94a139150675ded528c
vallurupallinavya <vallurupallinavya@gmail.com>
2024-09-17 16:53:27 +0100

    Initial commit

---
+++ src/env.ts
@@ -0,0 +1,18 @@
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const env = {
+  firebase : {
+    apiKey: "AIzaSyCM4d5FkA8KYcHpyvdGLZeEjlKbK7X8XBg",
+    authDomain: "drinksuperstore9.firebaseapp.com",
+    projectId: "drinksuperstore9",
+    storageBucket: "drinksuperstore9.appspot.com",
+    messagingSenderId: "1070528129816",
+    appId: "1:1070528129816:web:ef7dbba85b22a98dbecc6d",
+    measurementId: "G-SLSGGTLW71"
+  }
+
+
+  
+};
