552325b34134a35fd0aa13fad5a8fb561466ae61
vallurupallinavya <vallurupallinavya@gmail.com>
2024-09-17 17:17:58 +0100

    firebase configure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes, withViewTransitions()),
+    provideAnimations(),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
+      provideStorage(() => getStorage())
+    ]),
+  ],
 };
+
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,8 @@
+export const environment = {
 
-
-// Your web app's Firebase configuration
+    // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const env = {
-  firebase : {
+firebaseConfig : {
     apiKey: "AIzaSyCM4d5FkA8KYcHpyvdGLZeEjlKbK7X8XBg",
     authDomain: "drinksuperstore9.firebaseapp.com",
     projectId: "drinksuperstore9",
@@ -12,7 +11,5 @@ export const env = {
     appId: "1:1070528129816:web:ef7dbba85b22a98dbecc6d",
     measurementId: "G-SLSGGTLW71"
   }
-
-
   
 };
